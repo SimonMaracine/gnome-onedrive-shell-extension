0838e3fef656071e7fc929e2919dea28b908e85a
SimonMaracine <simonmaracine@gmail.com>
2023-11-02 19:07:50 +0200

    Try to make onedrive synchronization work

--- extension.js
+++ extension.js
@@ -1,8 +1,36 @@
-const { GObject, St } = imports.gi;
+const { GObject, St, Clutter, Gio } = imports.gi;
 
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+
+function onedriveSynchronize(onedriveDirectory) {
+    const cd = new Gio.Subprocess(["cd", onedriveDirectory, null], Gio.SubprocessFlags.NONE);
+
+    if (cd === null) {
+        return false;
+    }
+
+    cd.wait(new Gio.Cancellable());
+
+    if (cd.get_exit_status() !== 0) {
+        return false;
+    }
+
+    const onedrive = new Gio.Subprocess(["onedrive", "--synchronize", null], Gio.SubprocessFlags.NONE);
+
+    if (onedrive === null) {
+        return false;
+    }
+
+    onedrive.wait(new Gio.Cancellable());
+
+    if (onedrive.get_exit_status() !== 0) {
+        return false;
+    }
+
+    return true;
+}
 
 const Indicator = GObject.registerClass(
     class Indicator extends PanelMenu.Button {
@@ -11,13 +39,24 @@ const Indicator = GObject.registerClass(
 
             this.add_child(
                 new St.Label({
-                    text: "OneDrive"
+                    text: "OneDrive",
+                    y_align: Clutter.ActorAlign.CENTER
                 })
             );
 
-            let item = new PopupMenu.PopupMenuItem("Synchronize");
+            const item = new PopupMenu.PopupMenuItem("Synchronize");
             item.connect("activate", () => {
+                Main.notify("Working?");
 
+                try {
+                    if (!onedriveSynchronize("/home/OneDrive")) {
+                        Main.notify("Failed to synchronize");
+                    } else {
+                        Main.notify("Successfully synchronized");
+                    }
+                } catch (e) {
+                    Main.notify(e.toString());
+                }
             });
             this.menu.addMenuItem(item);
         }
